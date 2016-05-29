c9ad2fe3c7b09f64e059ec12d37dec89f11bcf16
serj-lunev <sergej.lunev@phystech.edu>
2016-05-29 20:52:32 +0300

    minor changes

--- Client.java
+++ Client.java
@@ -4,18 +4,23 @@ import java.net.*;
 class Client{
 	public static void main(String argv[]) throws Exception{
 		String sentence;
-		String exit = "EXIT" + '\n';
+		String serverIP;
+		int serverPort = 6789;
 		String modifiedSentence;
 		BufferedReader inFromUser = new BufferedReader (new InputStreamReader(System.in));
-		Socket clientSocket = new Socket("10.55.164.215", 6789);
+
+		System.out.printf("input server IP\n");
+		serverIP = inFromUser.readLine();
+		Socket clientSocket = new Socket(serverIP, serverPort);
 		DataOutputStream outToServer = new DataOutputStream (clientSocket.getOutputStream());
 		BufferedReader inFromServer = new BufferedReader (new InputStreamReader(clientSocket.getInputStream()));
+		
 		System.out.printf("input something\n");
 		sentence = inFromUser.readLine();
-			outToServer.writeBytes(sentence + '\n');
-			modifiedSentence = inFromServer.readLine();
-			System.out.println("FROM SERVER: " + modifiedSentence);
-			sentence = inFromUser.readLine();
+		outToServer.writeBytes(sentence + '\n');
+		modifiedSentence = inFromServer.readLine();
+		System.out.println("FROM SERVER: " + modifiedSentence);
+		sentence = inFromUser.readLine();
 		clientSocket.close();
 	}
 }
